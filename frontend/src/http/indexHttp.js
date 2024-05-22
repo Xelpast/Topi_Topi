c148d9f2b883213ee07975f0961ec9ea0df2d941
Xelpast <sasha.tarasov1609@gmail.com>
2024-05-22 20:38:44 +0300

    update URL

--- frontend/src/http/indexHttp.js
+++ frontend/src/http/indexHttp.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 // https://topi-topi.up.railway.app/
 // http://localhost:5000/
-const AXIOS_URL = 'http://localhost:5000/';
+const AXIOS_URL = 'https://topi-topi.up.railway.app/';
 
 const $host = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://topi-topi.up.railway.app/'
 })
 
 const $authHost = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://topi-topi.up.railway.app/'
 })
 
 const authInterceptor = config => {
